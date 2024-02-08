1f49ded4956482e004179fc3db43a2160add2bc1
Adrian4891 <ernebarra36@gmail.com>
2024-02-08 10:55:04 -0300

    Db_deploy

--- Triviar-App/Api/src/dbConnection.js
+++ Triviar-App/Api/src/dbConnection.js
@@ -1,8 +1,8 @@
 require("dotenv").config()
 const mongoose = require("mongoose");
-const {MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_PORT, MONGO_DB} = process.env;
+const {MONGODB_URL} = process.env;
 
-const uriDb = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const uriDb = MONGODB_URL;
 
 const dbConnect = async () => {
     try {
