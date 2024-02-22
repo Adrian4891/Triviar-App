5f807f7bc8772a2f22c35d6073ed6bad340d2729
Adrian4891 <ernebarra36@gmail.com>
2024-02-22 16:46:21 -0300

    :/

--- Triviar-App/Client/src/components/welcome/Welcome.jsx
+++ Triviar-App/Client/src/components/welcome/Welcome.jsx
@@ -5,30 +5,41 @@ import { URL_BASE } from "../../utils";
 import axios from "axios";
 import imgLogo from "../../images/headerLogo.svg";
 const Welcome = () => {
-  const userId = Cookies.get("userId");
-  const navigate = useNavigate("/");
+    const userId = Cookies.get("userId");
+    const navigate = useNavigate("/");
 
-  const getProfile = async () => {
-    const { data } = await axios(`${URL_BASE}/profile/${userId}`);
-    console.log(data);
-    if (data[0]?.userName) {
-      return navigate("/");
-    } else {
-      return navigate("/profile/create");
+    const getProfile = async () => {
+        try {
+            const { data } = await axios(`${URL_BASE}/profile/${userId}`);
+            if(data[0]?.userName){
+                return navigate("/");
+            }
+            else {
+                return  navigate("/profile/create");
+            } 
+        } catch (error) {
+            console.log(error);
+        }
     }
-  };
+    
+    useEffect(()=>{
+        setTimeout(() => {
+            getProfile();  
+        }, 1000);
+       
+    },[]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      getProfile();
-    }, 1000);
-  }, []);
-
-  return (
-    <div className="flex justify-center items-center bg-dark w-full h-screen flex-col gap-3">
-      <p className=" text-white font-bold text-4xl">Bienvenido...</p>
-      <img src={imgLogo} alt="image-logo" className="w-32" />
-    </div>
-  );
-};
+    return(
+        <div className="flex justify-center items-center bg-dark w-full h-screen flex-col gap-3">
+            <p className=" text-white font-bold text-4xl">
+                Bienvenido...
+            </p>
+            <img
+            src={imgLogo}
+            alt="image-logo"
+            className="w-32"
+            />
+        </div>
+    )
+}
 export default Welcome;
--- Triviar-App/Client/src/utils.js
+++ Triviar-App/Client/src/utils.js
@@ -1,2 +1,2 @@
-export const URL_BASE = "https//triviarapi.onrender.com";
+export const URL_BASE = "https://triviarapi.onrender.com";
 
